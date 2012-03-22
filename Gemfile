ea3df807815e11569c1087eaac0530b9ead60204
Sarah Adams <sarahsponda902@gmail.com>
2012-03-22 08:48:16 +0800

    add users

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'sqlite3'
 
 gem 'hirb'
 
+gem 'devise'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-RMTest::Application.config.session_store :cookie_store, key: '_RM - Test_session'
+RMTest::Application.config.session_store :cookie_store, :key => '_RM - Test_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
